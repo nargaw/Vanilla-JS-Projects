8fadc3bb822e6305e380c5cdb197a644ba86704a
Nate A <59675817+nargaw@users.noreply.github.com>
2021-05-18 07:06:35 -0400

    Update script.js

--- Connect 4/script.js
+++ Connect 4/script.js
@@ -8,21 +8,21 @@
 //diagonal iterate through diagnoal piece to see if there is a consevutive four of 1 or 2 - announce winner
 //once winner is announced - button to reload page 
 
-const array = [
-    [0, 0, 0, 1, 0, 0, 1],
-    [0, 44, 0, 0, 0, 1, 0],
-    [0, 0, 1, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0]
-];
+var array = [
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0]
+        ];
 
 const players = [1, 2];
 const current = Math.floor(Math.random() * players.length);
 
 window.onload = function play() {
     console.log(players[current]);
-    if (players[current] == 1){
+    if (players[current] === 1){
         document.getElementById('p1').classList.toggle('p1on');
     } else {
         document.getElementById('p2').classList.toggle('p2on');
@@ -30,15 +30,47 @@ window.onload = function play() {
 }
 
 function game(clicked_id){
-    console.log(document.getElementById(clicked_id).id);
     let column = document.getElementById(clicked_id).id;
-    console.log(column);
+    //console.log(array[4][column-1]);
+    //console.log(array);
     for (let i = 0; i<= 5; i++){
-        if (array[i][column-1] !== 0){
-            console.log(array[i][column-1])
-            break
+        if (array[i][column-1] === 1 || array[i][column-1] === 2){
+            //console.log(array[i-1][column-1])
+            //console.log(i);
+            //console.log(column);
+            console.log(document.getElementById('r'+i+'c'+column).id);
+            
+            console.log(array);
+            if (players[current] === 1){
+                array[i-1][column-1] = players[current];
+                document.getElementById('r'+i+'c'+column).classList.replace('space', 'one');
+                
+                console.log(players[current]);
+                players[current] = 2;
+                document.getElementById('p2').classList.toggle('p2on');
+                break;
+            } else {
+                array[i-1][column-1] = players[current];
+                document.getElementById('r'+i+'c'+column).classList.replace('space', 'two');
+                console.log(players[current]);
+                players[current] = 1;
+                document.getElementById('p1').classList.toggle('p1on');
+                break;
+            }
         } else {
-            console.log('all zeros')
-        }
-    }
+            if (players[current] === 1){
+                array[5][column-1] = players[current];
+                document.getElementById('r'+6+'c'+column).classList.replace('space', 'one');
+                document.getElementById('p1').classList.toggle('p1on');
+                console.log(players[current]);
+                players[current] = 2;
+            } else {
+                array[5][column-1] = players[current];
+                document.getElementById('r'+6+'c'+column).classList.replace('space', 'two');
+                document.getElementById('p2').classList.toggle('p2on');
+                console.log(players[current]);
+                players[current] = 1;
+            }
+        } 
+    }  
 }
