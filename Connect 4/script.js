26f8206f67c50dc2fecbfbc71d7e1f70a0d231db
Nate A <59675817+nargaw@users.noreply.github.com>
2021-05-19 10:54:52 -0400

    Update script.js

--- Connect 4/script.js
+++ Connect 4/script.js
@@ -19,58 +19,60 @@ var array = [
 
 const players = [1, 2];
 const current = Math.floor(Math.random() * players.length);
+console.log(players[current]);
 
-window.onload = function play() {
-    console.log(players[current]);
-    if (players[current] === 1){
-        document.getElementById('p1').classList.toggle('p1on');
-    } else {
-        document.getElementById('p2').classList.toggle('p2on');
-    }
-}
+// window.onload = function play() {
+//     if (players[current] === 1){
+//         document.getElementById('p1').classList.toggle('p1on');
+//     } else {
+//         document.getElementById('p2').classList.toggle('p2on');
+//     } return;
+// }
 
 function game(clicked_id){
     let column = document.getElementById(clicked_id).id;
-    //console.log(array[4][column-1]);
-    //console.log(array);
-    for (let i = 0; i<= 5; i++){
-        if (array[i][column-1] === 1 || array[i][column-1] === 2){
-            //console.log(array[i-1][column-1])
-            //console.log(i);
-            //console.log(column);
-            console.log(document.getElementById('r'+i+'c'+column).id);
-            
-            console.log(array);
+    console.log(column);
+    for (let i=0; i<=5; i++){
+        if (array[5][column-1] === 0) {
             if (players[current] === 1){
-                array[i-1][column-1] = players[current];
-                document.getElementById('r'+i+'c'+column).classList.replace('space', 'one');
-                
-                console.log(players[current]);
-                players[current] = 2;
-                document.getElementById('p2').classList.toggle('p2on');
-                break;
-            } else {
-                array[i-1][column-1] = players[current];
-                document.getElementById('r'+i+'c'+column).classList.replace('space', 'two');
-                console.log(players[current]);
-                players[current] = 1;
-                document.getElementById('p1').classList.toggle('p1on');
-                break;
-            }
-        } else {
-            if (players[current] === 1){
-                array[5][column-1] = players[current];
+                console.log('zero1');
+                array[5][column-1] = 1;
                 document.getElementById('r'+6+'c'+column).classList.replace('space', 'one');
-                document.getElementById('p1').classList.toggle('p1on');
-                console.log(players[current]);
+                console.log(array);
                 players[current] = 2;
+                document.getElementById('p2').classList.add('p2on');
+                document.getElementById('p1').classList.remove('p1on');
+                return;
             } else {
-                array[5][column-1] = players[current];
+                console.log('zero2');
+                array[5][column-1] = 2;
                 document.getElementById('r'+6+'c'+column).classList.replace('space', 'two');
-                document.getElementById('p2').classList.toggle('p2on');
-                console.log(players[current]);
+                console.log(array);
                 players[current] = 1;
+                document.getElementById('p2').classList.remove('p2on');
+                document.getElementById('p1').classList.add('p1on');
+                return;
+                }
+        } else if (array[i][column-1] === 1 || (array[i][column-1] === 2)){
+            if(players[current] === 1){
+                console.log('one');
+                array[i-1][column-1] = 1;
+                document.getElementById('r'+(i)+'c'+column).classList.replace('space', 'one');
+                console.log(array);
+                players[current] = 2;
+                document.getElementById('p2').classList.add('p2on');
+                document.getElementById('p1').classList.remove('p1on');
+                return;
+            } else {
+                console.log('two');
+                array[i-1][column-1] = 2;
+                document.getElementById('r'+(i)+'c'+column).classList.replace('space', 'two');
+                console.log(array);
+                players[current] = 1;
+                document.getElementById('p2').classList.remove('p2on');
+                document.getElementById('p1').classList.add('p1on');
+                return;
             }
-        } 
-    }  
+        }
+    }
 }
