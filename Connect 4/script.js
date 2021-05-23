6a13c9a6a7ad976d15c6d54ccccf35f50c04986e
Nate A <59675817+nargaw@users.noreply.github.com>
2021-05-23 18:52:30 -0400

    Update script.js

--- Connect 4/script.js
+++ Connect 4/script.js
@@ -28,7 +28,6 @@ window.onload = function play() {
          document.getElementById('p2').classList.toggle('p2on');
      } return;
 }
-
 function game(clicked_id){
     let column = document.getElementById(clicked_id).id;
     console.log(column);
@@ -74,5 +73,65 @@ function game(clicked_id){
                 return;
             }
         }
+    }
+}
+function checkWinner(clicked_id){
+    //console.log(array.length);
+    var str = [];
+    var str1 = []; 
+    var str2 = [];
+    var str3 = [];
+    var str4 = [];
+    var str5 = [];
+    var str6 = [];
+    var c = [];
+    let column = document.getElementById(clicked_id).id;
+    for (let x = 0; x <= array.length -1; x++){
+        c.push(array[x][column-1]).toString();
+        for (let y = 0; y <= array[x].length-1; y++){
+            str.push(array[x][y]);
+        }
+    }
+    
+    for (let i = 0; i<=6; i++){
+        str1.push(str[0 + i]).toString();
+        str2.push(str[7 + i]).toString();
+        str3.push(str[14 + i]).toString();
+        str4.push(str[21 + i]).toString();
+        str5.push(str[28 + i]).toString();
+        str6.push(str[35 + i]).toString();
+    }
+    let s6 = '';
+    s6 = str6.join('');
+    console.log(s6);
+    let s5 = '';
+    s5 = str5.join('');
+    let s4 = '';
+    s4 = str4.join('');
+    let s3 = '';
+    s3 = str3.join('');
+    let s2 = '';
+    s2 = str2.join('');
+    let s1 = '';
+    s1 = str1.join('');
+    if (s6.match(/1111/g)||s5.match(/1111/g)||s4.match(/1111/g)||s3.match(/1111/g)||s2.match(/1111/g)||s1.match(/1111/g)){
+        console.log('winner is player one');
+        document.getElementById('w1').innerHTML = "Player 1 - Winner";
+    } else if (s6.match(/2222/g)||s5.match(/2222/g)||s4.match(/2222/g)||s3.match(/2222/g)||s2.match(/2222/g)||s1.match(/2222/g)){
+        console.log('winner is player two');
+        document.getElementById('w2').innerHTML = "Player 2 - Winner";
+    }
+
+    let cStr = '';
+    cStr = c.join('');
+    console.log(cStr);
+    if (cStr.match(/1111/g)){
+        console.log('winner is player 1 ');
+        document.getElementById('w1').innerHTML = "Player 1 - Winner";
+    } else if (cStr.match(/2222/g)){
+        console.log('winner is player 2');
+        document.getElementById('w2').innerHTML = "Player 2 - Winner";
+    } else {
+        return;
     }
 }
