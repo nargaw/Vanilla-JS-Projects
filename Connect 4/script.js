5a4b986ad2fb40acb4b46ca2feb639d91333760a
Nate A <59675817+nargaw@users.noreply.github.com>
2021-05-25 05:54:41 -0400

    Update script.js

--- Connect 4/script.js
+++ Connect 4/script.js
@@ -21,13 +21,14 @@ const players = [1, 2];
 const current = Math.floor(Math.random() * players.length);
 console.log(players[current]);
 
-window.onload = function play() {
-     if (players[current] === 1){
-         document.getElementById('p1').classList.toggle('p1on');
-     } else {
-         document.getElementById('p2').classList.toggle('p2on');
-     } return;
-}
+// window.onload = function play() {
+//     if (players[current] === 1){
+//         document.getElementById('p1').classList.toggle('p1on');
+//     } else {
+//         document.getElementById('p2').classList.toggle('p2on');
+//     } return;
+// }
+
 function game(clicked_id){
     let column = document.getElementById(clicked_id).id;
     console.log(column);
@@ -75,8 +76,9 @@ function game(clicked_id){
         }
     }
 }
+
 function checkWinner(clicked_id){
-    //console.log(array.length);
+    //check horizontal winner
     var str = [];
     var str1 = []; 
     var str2 = [];
@@ -122,6 +124,86 @@ function checkWinner(clicked_id){
         document.getElementById('w2').innerHTML = "Player 2 - Winner";
     }
 
+    //check diagonal winner
+    var yLen = array.length;
+    var xLen = array[0].length;
+    var maxLen = Math.max(xLen, yLen);
+    var temp=[];
+    var temp2=[];
+
+    for (var a = 0; a<= (2 * (maxLen - 1)); a++){
+        
+        for (var y = yLen - 1; y >= 0; y--){
+            var x = a - y;
+            if (x >=0 && x < xLen){
+                temp.push(array[y][x]);
+            }
+            var z = a - (yLen - y -1);
+            if (z >= 0 && z < xLen){
+                temp2. push(array[y][z])
+            }
+        }   
+    }
+    console.log(temp);
+    console.log(temp2);
+        //diagonal top left to bottom right: temp1
+    var diag1 = [];
+    var diag2 = [];
+    var diag3 = [];
+    var diag4 = [];
+    var diag5 = [];
+    var diag6 = [];
+    var dop1 = [];
+    var dop2 = [];
+    var dop3 = [];
+    var dop4 = [];
+    var dop5 = [];
+    var dop6 = [];
+    for (let i = 0; i<=6; i++){
+        diag1 = temp.slice(6, 10).join('');
+        diag2 = temp.slice(10, 15).join('');
+        diag3 = temp.slice(15, 21).join('');
+        diag4 = temp.slice(21, 27).join('');
+        diag5 = temp.slice(27, 32).join('');
+        diag6 = temp.slice(32, 36).join('');
+        dop1 = temp2.slice(6, 10).join('');
+        dop2 = temp2.slice(10, 15).join('');
+        dop3 = temp2.slice(15, 21).join('');
+        dop4 = temp2.slice(21, 27).join('');
+        dop5 = temp2.slice(27, 32).join('');
+        dop6 = temp2.slice(32, 36).join('');
+    }
+    // console.log(diag1);
+    // console.log(diag2);
+    // console.log(diag3);
+    // console.log(diag4);
+    // console.log(diag5);
+    // console.log(diag6);
+
+    console.log(dop1);
+    console.log(dop2);
+    console.log(dop3);
+    console.log(dop4);
+    console.log(dop5);
+    console.log(dop6);
+
+    if (diag1.match(/1111/g)||diag2.match(/1111/g)||diag3.match(/1111/g)||diag4.match(/1111/g)||diag5.match(/1111/g)||diag6.match(/1111/g)){
+        console.log('winner is player one');
+        document.getElementById('w1').innerHTML = "Player 1 - Winner";
+    } else if (diag1.match(/2222/g)||diag2.match(/2222/g)||diag3.match(/2222/g)||diag4.match(/2222/g)||diag5.match(/2222/g)||diag6.match(/2222/g)) {
+        console.log('winner is player two');
+        document.getElementById('w2').innerHTML = "Player 2 - Winner";
+    }
+
+    if (dop1.match(/1111/g)||dop2.match(/1111/g)||dop3.match(/1111/g)||dop4.match(/1111/g)||dop5.match(/1111/g)||dop6.match(/1111/g)){
+        console.log('winner is player one');
+        document.getElementById('w1').innerHTML = "Player 1 - Winner";
+    } else if (dop1.match(/2222/g)||dop2.match(/2222/g)||dop3.match(/2222/g)||dop4.match(/2222/g)||dop5.match(/2222/g)||dop6.match(/2222/g)) {
+        console.log('winner is player two');
+        document.getElementById('w2').innerHTML = "Player 2 - Winner";
+    }
+
+    //check vertical winner
     let cStr = '';
     cStr = c.join('');
     console.log(cStr);
