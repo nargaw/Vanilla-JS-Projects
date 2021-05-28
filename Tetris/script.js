a7b1a941c26ae13210108d05f7abaa6c89eac9a7
Nate A <59675817+nargaw@users.noreply.github.com>
2021-05-28 05:47:10 -0400

    Create script.js

---
+++ Tetris/script.js
@@ -0,0 +1,10 @@
+//define a grid 12 X 18
+//define shapes T, L, J, square, rectangle, s and z
+//each shape has different color and grid 1-7
+//randomized shapes - give random shape after each shape
+//once a random shape is given display at the top of the tile gri
+//move shape down 1 spot per 1/2 a second
+//update display after every iteration
+//change grid from 0 to 7 based on shape and position
+//key press left, key press right, spacebar rotate and key press 
+//if the grid has any number 1-7, the piece can't go down anymore
